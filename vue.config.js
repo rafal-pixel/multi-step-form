c30ca49b143af0d7a1924f240a5d1c8c2275bd5a
robotamozepoczekac <robotamozepoczekac@gmail.com>
2023-04-04 16:38:24 +0200

    remove dist

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/multi-step-deploy/dist/'
+  ? '/multi-step-deploy/'
   : '/',
   transpileDependencies: true,
 });
